c3d2e724aac375047930fee77bbac8f13100446e
Ruofan Wong <meimprankster@gmail.com>
2023-11-15 15:34:25 +0800

    add error handling

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
-    !req.body.name && res.status(400).json({ error: "Missing name" });
+    req.body.name === "" && res.status(401).json({ error: "Missing name!" });
     res.status(200).json({ msg: `Hello, ${req.body.name}` });
   } else {
     res.status(200).json({ msg: "Hello" });
--- src/pages/hello.js
+++ src/pages/hello.js
@@ -7,20 +7,41 @@ async function sayHelloAsync(text) {
     body: JSON.stringify({ name: text }),
   });
   const data = await response.json();
-  return data;
+  if (!response.ok) {
+    return {
+      code: response.status,
+      error: await data.error,
+    };
+  }
+  return {
+    code: response.status,
+    msg: data.msg,
+  };
 }
 
 const HelloPage = () => {
   const [text, setText] = useState("");
-  const [received, setReceived] = useState("");
+  const [received, setReceived] = useState({
+    code: null,
+    msg: null,
+    error: null,
+  });
 
   return (
     <main className="main-container">
       <h1>Hello Page</h1>
-      <div>
-        <p>Received:</p>
-        <p>{received}</p>
-      </div>
+      {received.msg && (
+        <div className="bg-green-700 text-white p-2 m-2">
+          <p>{received.code} Received:</p>
+          <p>{received.msg}</p>
+        </div>
+      )}
+      {received.error && (
+        <div className="bg-red-700 text-white p-2 m-2">
+          <p>{received.code} Error:</p>
+          <p>{received.error}</p>
+        </div>
+      )}
       <div className="input-group">
         <input
           type="text"
@@ -31,8 +52,8 @@ const HelloPage = () => {
         />
         <button
           onClick={async () => {
-            const { msg } = await sayHelloAsync(text);
-            setReceived(msg);
+            const data = await sayHelloAsync(text);
+            setReceived(data);
           }}
         >
           Say hello
